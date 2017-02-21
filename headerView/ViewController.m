ad50d05a163e3cc36e63cfc5eaf2cfc312a1c795
朱明灿 <zhumingcan@zhumingcandeiMac.local>
2017-02-21 17:25:53 +0800

    headerView

--- headerView/ViewController.m
+++ headerView/ViewController.m
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
 
+@property(strong,nonatomic)UITableView * tableView;
+@property(strong,nonatomic)UIView * headerView;
+@property(strong,nonatomic)UIImageView * imageView;
 @end
 
 @implementation ViewController
@@ -17,8 +20,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    [self.view addSubview:_tableView];
+    
+    _headerView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 200)];
+    _imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, _headerView.frame.size.width, _headerView.frame.size.height)];
+    _imageView.image = [UIImage imageNamed:@"op"];
+    [_headerView addSubview:_imageView];
+    _tableView.tableHeaderView = _headerView;
+
 }
 
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString * iden = @"iden";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:iden];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:iden];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%i",indexPath.row];
+    return cell;
+}
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    CGFloat offset = scrollView.contentOffset.y;
+       if (offset>0) {
+        CGRect frame = _imageView.frame;
+        frame.origin.y = MAX(offset*0.5, 0);
+        _imageView.frame = frame;
+        _headerView.clipsToBounds = YES;
+    }else{
+        CGFloat delta = 0.0f;
+        CGRect rect = CGRectMake(0, 0, self.view.frame.size.width, 240);
+        delta = fabs(MIN(0.0f, offset));
+        rect.origin.y -= delta;
+        rect.size.height += delta;
+        _imageView.frame = rect;
+        _headerView.clipsToBounds = NO;
+    }
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
